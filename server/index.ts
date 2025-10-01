d737341dfa26bc9c0f631c32cb9a3fad9574a5ab
lovisgod <ayooluwa.olosunde@gmail.com>
2025-10-01 22:44:59 +0100

    feature: load sermons from db

--- server/index.ts
+++ server/index.ts
@@ -62,7 +62,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen(
-    port,"127.0.0.1", () => {
+    port,"0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
 })();
